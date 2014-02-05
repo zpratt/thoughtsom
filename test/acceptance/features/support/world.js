34ba189f8d7961595515ee7d7aecd5ac7a178cf6
zach pratt <zach@attackofzach.com>
2014-02-05 00:41:59 -0600

    Temporariy fix for jshint errors. I need to figure out a better way to handle connecting and disconnecting from mongo in the BDD tests.

--- test/acceptance/features/support/world.js
+++ test/acceptance/features/support/world.js
@@ -1,17 +1,19 @@
 'use strict';
 
-//(function () {
+(function () {
     var mongoose = require('mongoose'),
         ThoughtModel = require('../../../../src/server/models/thought');
 
     var World = function World(callback) {
         this.createThought = function (callback) {
             var model;
-            mongoose.connect('mongodb://localhost:27017/thoughts', function (err) {
-//                if (err) {
-//                    console.log('error connecting to mongo to save');
-//                }
-            });
+            mongoose.connect('mongodb://localhost:27017/thoughts',
+                function (err) {
+                    if (err) {
+//                        console.log('error connecting to mongo to save');
+                    }
+                }
+            );
 
             model = new ThoughtModel({
                 title: 'This is an article',
@@ -20,20 +22,24 @@
 
             model.save(function (err, model) {
                 if (err) {
-                    console.log('error saving model' + JSON.stringify(model));
+                    console.log('error saving model: ' + JSON.stringify(model));
                 }
                 callback();
             });
         };
 
         this.clearDB = function clearDB(callback) {
-            mongoose.connect('mongodb://localhost:27017/thoughts', function (err) {
-//                if (err) {
-//                    console.log('error connecting to mongo to remove');
-//                }
-//                callback();
-            });
+            mongoose.connect('mongodb://localhost:27017/thoughts',
+                function (err) {
+                    if (err) {
+//                        console.log('error connecting to mongo to remove');
+                    }
+                }
+            );
             ThoughtModel.collection.remove(function (err) {
+                if(err) {
+//                    console.log('Error occurred removing: ' + err);
+                }
                 callback();
             });
         };
@@ -42,4 +48,4 @@
     };
 
     module.exports.World = World;
-//}());
+}());
