a15ba78e73cd8497f04abc605950945fab924498
zach pratt <zach@attackofzach.com>
2014-02-16 00:47:35 -0600

    Added the beginnings of a mongoose API stub which is needed to avoid partial mocking of the mongoose library.

---
+++ test/helpers/stubs/mongooseStub.js
@@ -0,0 +1,17 @@
+module.exports = (function () {
+    var casual = require('casual');
+
+    return {
+        mongoose: {
+            connect: function () { return; },
+            model: {
+                findById: function (id) { return id; }
+            },
+            Types: {
+                ObjectId: function ObjectId() {
+                    return casual.word;
+                }
+            }
+        }
+    };
+}());
