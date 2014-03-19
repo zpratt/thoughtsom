8a5d9dbff0aff51f7ef1f1897e49aacfa32661b4
zach pratt <zach@attackofzach.com>
2014-03-19 01:02:16 -0500

    added middleware config to serve static files from the public directory in the root of the project

--- src/server/app/server.js
+++ src/server/app/server.js
@@ -1,21 +1,26 @@
-'use strict';
+(function () {
+    'use strict';
 
-var express = require('express'),
-    thoughtRoute = require('./controllers/thought-route');
+    var express = require('express'),
+        thoughtRoute = require('./controllers/thought-route'),
+        CONFIG = require('config'),
+        publicDir = CONFIG.Default.serverRoot + '/../../public/';
 
-var app = express();
+    var app = express();
 
-exports.start = function (port) {
-    app.listen(port);
-    app.use(express.logger());
-    app.use(express.urlencoded());
-    app.use(express.json());
+    exports.start = function (port) {
+        app.listen(port);
+        app.use(express.static(publicDir));
+        app.use(express.logger());
+        app.use(express.urlencoded());
+        app.use(express.json());
 
-    console.log('Listening on port ' + port);
-};
+        console.log('Listening on port ' + port);
+    };
 
-exports.app = app;
+    exports.app = app;
 
-app.get('/thought/:id', thoughtRoute.getById);
-app.post('/thought', [express.json(), express.urlencoded()], thoughtRoute.create);
-app.put('/thought/:id', [express.json(), express.urlencoded()], thoughtRoute.update);
+    app.get('/thought/:id', thoughtRoute.getById);
+    app.post('/thought', [express.json(), express.urlencoded()], thoughtRoute.create);
+    app.put('/thought/:id', [express.json(), express.urlencoded()], thoughtRoute.update);
+}());
