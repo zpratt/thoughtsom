cd89104ce9799af3f5e685a42819c7012b2c26fc
zach pratt <zach@attackofzach.com>
2014-06-08 04:00:34 -0500

    merged bdd and unit tests into tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -109,6 +109,8 @@ module.exports = function (grunt) {
     grunt.registerTask('test', ['unit:server', 'unit:ui', 'bdd:server', 'bdd:ui']);
     grunt.registerTask('unit:server', ['mochacov:unit']);
     grunt.registerTask('unit:ui', ['karma:unit']);
+    grunt.registerTask('test:unit', ['unit:server', 'unit:ui']);
     grunt.registerTask('bdd:ui', ['karma:bdd']);
     grunt.registerTask('bdd:server', ['mochacov:bdd_server']);
+    grunt.registerTask('test:bdd', ['bdd:server', 'bdd:ui']);
 };
